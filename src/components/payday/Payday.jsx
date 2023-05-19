e7e0adb97ef96ae8849eb6d6df8cd97f44b8b8a2
Demna <kapanadze1987@gmail.com>
2023-05-19 16:40:11 +0400

    Adding Daypay section

--- src/components/payday/Payday.jsx
+++ src/components/payday/Payday.jsx
@@ -1,20 +1,33 @@
 import styles from './Payday.module.css';
+import payadayImg from './../../images/images/payday.jpg';
 
 const Payday = () => {
     return (
         <section className={styles.payday}>
             <div className={styles.container}>
                 <div className={styles.paydayContent}>
+                    <div className={styles.daypayImg}>
+                        <img src={payadayImg} alt="payadayImg" />
+                    </div>
                     <div className={styles.paydayText}>
                         <div className={styles.paydayTitle}>
                             <span className={styles.highlight}>
                                 <span>PAYDAY</span>
-                            </span>{' '}
+                            </span>
                             SALE NOW
+                            <div className={styles.paydayDescr}>
+                                Spend minimal $100 get 30% off voucher code for
+                                your next purchase
+                                <span className={styles.descrBold}>
+                                    1 June - 10 June 2021
+                                </span>
+                                *Terms & Conditions apply
+                            </div>
                         </div>
-                        <div className={styles.paydayDescr}>
-                            Spend minimal $100 get 30% off voucher code for your
-                            next purchase
+                        <div>
+                            <a href="#!" className={styles.paydayBtn}>
+                                SHOP NOW
+                            </a>
                         </div>
                     </div>
                 </div>
